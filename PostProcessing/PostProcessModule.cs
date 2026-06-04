3c1496821f2178e61c9bb08a45d0e9a9ccbe1c49
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-06-04 16:57:32 -0400

    afasf

--- PostProcessing/PostProcessModule.cs
+++ PostProcessing/PostProcessModule.cs
@@ -26,11 +26,11 @@ public abstract partial class PostProcessModule : SubViewport
     public virtual void Initialize(Vector2I renderSize)
     {
         RenderSize = renderSize;
-        Size = RenderSize;
+        Size = renderSize;
         TransparentBg = true;
         HandleInputLocally = false;
         RenderTargetClearMode = ClearMode.Always;
-        RenderTargetUpdateMode = UpdateMode.Once;
+        RenderTargetUpdateMode = UpdateMode.Always;
 
         OnInitialize();
     }
